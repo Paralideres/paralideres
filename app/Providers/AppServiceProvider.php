26eb685be9375e779a64a5e0167bf409d849fdb6
animeshshuvro <animeshshuvro@gmail.com>
2017-11-27 01:31:16 +0600

    image path problem solved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        Schema::defaultStringLength(191);
         View::composer('layouts.common.footer', function ($view) {
             $categoriesCollections = Category::with(['collections' => function($q){
 //                    $q->limit(20);
--- resources/views/layouts/common/social.blade.php
+++ resources/views/layouts/common/social.blade.php
@@ -1,21 +1,41 @@
-<section class="social_share_area">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="social_inner text-center">
-                    <h3><span><img src="{{asset('images/social-3.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.facebook.com/paralideres">Siguenos!</a></h3>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="social_inner social_inner_2 text-center">
-                    <h3><span><img src="{{asset('images/social-2.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.twitter.com/paralideres">Siguenos!</a></h3>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="social_inner social_inner_3 text-center">
-                    <h3><span><img src="{{asset('images/social-1.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.youtube.com/paralideres">Siguenos!</a></h3>
-                </div>
-            </div>
-        </div>
-    </div>
+<section class="social_share_area">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-4">
+
+                <div class="social_inner text-center">
+
+                    <h3><span><img src="{{asset('images/social-3.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.facebook.com/paralideres">Siguenos!</a></h3>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-4">
+
+                <div class="social_inner social_inner_2 text-center">
+
+                    <h3><span><img src="{{asset('images/social-2.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.twitter.com/paralideres">Siguenos!</a></h3>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-4">
+
+                <div class="social_inner social_inner_3 text-center">
+
+                    <h3><span><img src="{{asset('images/social-1.png')}}" alt=""></span>9350 amigos en Face <a href="https://www.youtube.com/paralideres">Siguenos!</a></h3>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </section>  
